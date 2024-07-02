7aefdc463f81669a9905dec90fcfaf38f66cddde
WieszczY <wieszczy85@gmail.com>
2024-07-02 20:15:38 +0200

    Wysyłanie wiadomości

--- src/main/java/pl/mymc/MyDiscordBot.java
+++ src/main/java/pl/mymc/MyDiscordBot.java
@@ -1,12 +1,31 @@
 package pl.mymc;
 
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import javax.security.auth.login.LoginException;
 
-public class MyDiscordBot {
-    public static void main(String[] args) {
-        JDABuilder.createDefault("token")
-                .setActivity(Activity.playing("1.. 2... 3... Próba bota!"))
+public class MyDiscordBot extends ListenerAdapter {
+    public static void main(String[] args) throws LoginException {
+
+        String token = "token";
+        JDABuilder.createDefault(token)
+                .addEventListeners(new MyDiscordBot())
                 .build();
+    }
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        Message message = event.getMessage();
+        String content = message.getContentRaw();
+
+        if (content.equals("!ping")) {
+            event.getChannel().sendMessage("pong").queue();
+        }
+
+        if (content.toLowerCase().contains("test")) {
+            message.delete().queue();
+        }
     }
 }
